816503aefcbe7146301aae9862be97aed961da92
MartinMarrerooo <martin.marrero@estudiantes.utec.edu.uy>
2025-10-07 03:37:01 -0300

    Cierre de sesion creado e implementado

---
+++ src/main/java/com/culturarteWeb/servlets/CierreDeSesionServlet.java
@@ -0,0 +1,30 @@
+package com.culturarteWeb.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/cierreSesion")
+public class CierreDeSesionServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false); // false: no crear si no existe
+        if (session != null) {
+            session.invalidate();
+        }
+
+        request.setAttribute("mensaje", "Has cerrado sesión correctamente.");
+
+        // Redirigimos al JSP de cierre de sesión
+        request.getRequestDispatcher("/cierreSesion.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
